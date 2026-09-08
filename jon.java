519924fef89bf0ca693305f557c57d23c19bc561
Vivek Kashyap <vivekkashyap8299@gmail.com>
2026-09-08 18:05:18 +0530

    add notes pdf of java

--- jon.java
+++ jon.java
@@ -2,7 +2,25 @@
 public class jon {
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3};
-    
+        int arr[] = {1, 2, 3, 4, 5};
+        int pivot = -1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                pivot = i;
+                System.out.println(pivot);
+                break;
+            }
+        }
+        if (pivot == -1) {
+            for (int i = arr.length - 1; i >= 0; i--) {
+                System.out.print(arr[i] + " ");
+            }
+            return;
+        }
+        // int min = Integer.MAX_VALUE;
+        // for (int i = pivot; i < arr.length; i++) {
+        //     min = Math.min(min, arr[i]);
+        // }
+        System.out.println(pivot);
     }
 }
